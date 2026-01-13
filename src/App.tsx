e5c5ed4dacb26242af56f7a0f2b71064191d93f5
Fabio Pacheco <pachecofc@msn.com>
2026-01-13 13:26:41 -0300

    Move container

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá, Mundo 1
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>Este é o nosso primeiro projeto React.</p>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
     </>
   );
 }
