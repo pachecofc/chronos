46f26a0b0d4512978be8d41c6d98e1baf5e3711c
Fabio Pacheco <pachecofc@msn.com>
2026-01-13 11:49:00 -0300

    Create heading component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading />
+      <Heading>
+        Olá, Mundo 1
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>Este é o nosso primeiro projeto React.</p>
     </>
   );
